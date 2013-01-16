7c71f246a75dfe7dbb9c96256de04efc4e7451c6
Uzair Bukhari <uahmed@folio3.com>
2013-01-16 13:10:47 +0500

    Jquery Asignment

---
+++ JavaScriptPrac/JavaScripts/JSONpractice.js
@@ -0,0 +1,89 @@
+﻿var MenuDetail = {
+    Height: 50,
+    Width: 100,
+    Padding: 25,
+    Style: {
+        Font: "Segio UI",
+        Size: 13
+    },
+    Title: "Yahoo"
+};
+
+var menu = {
+    width: 300,
+    height: 200,
+    title: "Menu"
+};
+
+var multiplyNumeric = function(MenuObj) {
+    for (key in MenuObj) {
+        if (isNumeric(MenuObj[key]))
+        {
+            MenuObj[key] = MenuObj[key] * 2;
+        }
+    }
+}
+
+var isNumeric = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n)
+}
+
+
+//var ladder = {
+//    step: 0,
+//    up: function () {
+//        this.step++
+//        //return this
+//    },
+//    down: function () {
+//        this.step--
+//        return this
+//    },
+//    showStep: function () {
+//        alert(this.step)
+//        return this
+//    }
+//};
+
+//ladder.up().up().up().up().up().up().up().up().down().up().down().showStep();
+
+function Info(val) {
+    this.name = val;
+    this.setName = function (val) {
+        this.name = val;
+    }
+    this.getName = function () {
+        alert(this.name);
+    }
+    return {
+        Name: this.name,
+        GetName: this.getName,
+        SetName: this.setName
+    }
+};
+
+
+var i = new Info("uzair");
+alert(i.Name);
+i.SetName("ali");
+i.GetName();
+
+//alert(Info.Name);
+//Info.getName();
+//Info.displayName();
+
+//multiplyNumeric (menu);
+
+//for (key in menu) {
+//    var c = menu[key];
+//    //alert(c);
+//}
+
+//alert("end");
+//menu.width = 500;
+//alert(menu.width);​​​​​​​​​​​​
+
+var a = 'Uzair';
+
+var func = function () {
+}
---
+++ JavaScriptPrac/JavaScripts/MyValidator.js
@@ -0,0 +1,112 @@
+﻿
+var namespace = {};
+namespace.Util = {};
+
+namespace.Util.MyValidatorClass = (function () {
+    var requiredField_Validation = function (text) {
+        var reg = new RegExp('^[\s]*[\s]*$');
+        return !reg.test(text);
+    }
+    var numeric_Validation = function (text) {
+        if (!requiredField_Validation(text))
+            return false;
+        var reg = new RegExp('^\\d*$');
+        return reg.test(text);
+    }
+    var alphaNumeric_Validation = function (text) {
+        if (!requiredField_Validation(text))
+            return false;
+        var reg = new RegExp('^[a-zA-Z0-9_]*$');
+        return reg.test(text);
+    }
+    var email_Validation = function (text) {
+        if (!requiredField_Validation(text))
+            return false;
+        var reg = new RegExp("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        return reg.test(text);
+    }
+    var GPA_Validation = function (text) {
+        if (!requiredField_Validation(text))
+            return false;
+        var reg = new RegExp("^(?:[0-3](\.\d\d?)?)|4(\.0)?$");
+        return reg.test(text);
+    }
+    var FormValidator_Onclick = function () {
+        var elements = document.forms[0].elements;
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            if (element.type == 'text') {
+                namespace.Util.MyValidatorClass.FormValidator(element);
+            }
+        }
+    }
+
+    var Form_Validator = function (Element) {
+        switch (Element.getAttribute("validationtype")) {
+            case 'required':
+                namespace.Util.Design.SetColorOnvalidation(requiredField_Validation(Element.value), Element);
+                break;
+            case 'numeric':
+                namespace.Util.Design.SetColorOnvalidation(numeric_Validation(Element.value), Element);
+                break;
+            case 'email':
+                namespace.Util.Design.SetColorOnvalidation(email_Validation(Element.value), Element);
+                break;
+            case 'alphanumeric':
+                namespace.Util.Design.SetColorOnvalidation(alphaNumeric_Validation(Element.value), Element);
+                break;
+            case 'gpa':
+                namespace.Util.Design.SetColorOnvalidation(GPA_Validation(Element.value), Element);
+                break;
+        }
+    }
+    return {
+        FormValidator: Form_Validator,
+        FormValidatorOnclick: FormValidator_Onclick
+    };
+})();
+
+//alert(namespace.Util.MyValidatorClass.emailValidation("suzairgmail.com"));
+
+$(function () {
+    var elements = $('input:text');
+    alert(elements.length);
+    for (var i = 0; i < elements.length; i++) {
+        (function (index) {
+            var element = elements[index];
+            element.value = 'hello';
+            alert(element.value);
+            element.onkeyup = function () {
+                namespace.Util.MyValidatorClass.FormValidator(element);
+            }
+        })(i);
+    }
+    //    $('input:text').onkeyup(function () {
+    //        namespace.Util.MyValidatorClass.FormValidator(this);
+    //    });
+});
+
+//window.onload = function () {
+//    var elements = document.forms[0].elements;
+//    for (var i = 0; i < elements.length; i++) {
+//        //alert(elements[i].getAttribute("validationtype"));
+//        (function (index) {
+//            var element = elements[index];
+//            //alert(element.type);
+//            if (element.type == 'text')
+//                element.onkeyup = function () {
+//                    namespace.Util.MyValidatorClass.FormValidator(element);
+//                }
+//            else if (element.type == 'button')
+//                element.onclick = function () {
+//                    namespace.Util.MyValidatorClass.FormValidatorOnclick();
+//                }
+//        })(i);
+//    }
+//}
+namespace.Util.Design = {
+    SetColorOnvalidation: function (Result, Element) {
+        var className = Result ? "ValidInput" : "InValidInput";
+        Element.setAttribute("class", className);
+    }
+};
